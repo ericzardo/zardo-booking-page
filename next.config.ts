16e0d6aca6287057359f59e540c50bee0774e392
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-05-23 20:15:41 -0300

    fix litte bugs on deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   basePath: "/booking",
-  assetPrefix: "/booking",
+  assetPrefix: "/booking/",
     reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,73 @@
-'use client';
+  'use client';
 
-import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useScrollToSection } from '@/hooks/useScrollToSection';
-import { LoadingScreen } from '@zardo/ui-kit/feedback';
-import { Linkedin, Instagram } from 'lucide-react';
-import { LanguageSwitcher } from '@/components/LanguageSwitcher';
-import switchLanguage from '@/lib/translate/service/switch';
+  import dynamic from 'next/dynamic';
+  import { useEffect, useState } from 'react';
+  import { useTranslation } from 'react-i18next';
+  import { useScrollToSection } from '@/hooks/useScrollToSection';
+  import { LoadingScreen } from '@zardo/ui-kit/feedback';
+  import { Linkedin, Instagram } from 'lucide-react';
+  import { LanguageSwitcher } from '@/components/LanguageSwitcher';
+  import switchLanguage from '@/lib/translate/service/switch';
 
-const Calendar = dynamic(() => import('@/components/Calendar'), { ssr: false });
-const About = dynamic(() => import('@/components/About'), { ssr: false });
-const Benefits = dynamic(() => import('@/components/Benefits'), { ssr: false });
-const FAQ = dynamic(() => import('@/components/FAQ'), { ssr: false });
-const CTA = dynamic(() => import('@/components/CTA'), { ssr: false });
-const Slogan = dynamic(() => import('@zardo/ui-kit/layout').then(mod => mod.Slogan), { ssr: false });
-const Footer = dynamic(() => import('@zardo/ui-kit/layout').then(mod => mod.Footer), { ssr: false });
+  const Calendar = dynamic(() => import('@/components/Calendar'), { ssr: false });
+  const About = dynamic(() => import('@/components/About'), { ssr: false });
+  const Benefits = dynamic(() => import('@/components/Benefits'), { ssr: false });
+  const FAQ = dynamic(() => import('@/components/FAQ'), { ssr: false });
+  const CTA = dynamic(() => import('@/components/CTA'), { ssr: false });
+  const Slogan = dynamic(() => import('@zardo/ui-kit/layout').then(mod => mod.Slogan), { ssr: false });
+  const Footer = dynamic(() => import('@zardo/ui-kit/layout').then(mod => mod.Footer), { ssr: false });
 
-const iconMap = {
-  instagram: <Instagram strokeWidth={2} className="size-6 text-white/60" />,
-  linkedin: <Linkedin strokeWidth={2} className="size-6 text-white/60" />,
-};
+  const iconMap = {
+    instagram: <Instagram strokeWidth={2} className="size-6 text-white/60" />,
+    linkedin: <Linkedin strokeWidth={2} className="size-6 text-white/60" />,
+  };
 
-export default function Page() {
-  const [isClient, setIsClient] = useState(false);
-  const { t, i18n } = useTranslation('home');
-  const scrollToSection = useScrollToSection();
+  export default function Page() {
+    const [isClient, setIsClient] = useState(false);
+    const { t, i18n } = useTranslation('home');
+    const scrollToSection = useScrollToSection();
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+    useEffect(() => {
+      setIsClient(true);
+    }, []);
 
-  useEffect(() => {
-    const storedLang = localStorage.getItem("preferredLanguage");
+    useEffect(() => {
+      const storedLang = localStorage.getItem("preferredLanguage");
 
-    if (storedLang && storedLang !== i18n.language) {
-      switchLanguage(storedLang as "en" | "pt");
-    }
-  }, [i18n.language]);
+      if (storedLang && storedLang !== i18n.language) {
+        switchLanguage(storedLang as "en" | "pt");
+      }
+    }, [i18n.language]);
 
-  const SOCIAL_LINKS = (t('social', { returnObjects: true }) as { platform: keyof typeof iconMap; url: string; label: string }[] || [])
-    .map((item) => ({ ...item, icon: iconMap[item.platform] }));
+    const SOCIAL_LINKS = (t('social', { returnObjects: true }) as { platform: keyof typeof iconMap; url: string; label: string }[] || [])
+      .map((item) => ({ ...item, icon: iconMap[item.platform] }));
 
-return (
-  <div className="overflow-hidden bg-brand-offwhite relative">
-    {!isClient && (<LoadingScreen />)}
-    {isClient && (
-      <>
-        <LanguageSwitcher />
-        <Slogan
-          title={t('header.title')}
-          description={t('header.description')}
-          borderRadius="bottom"
-          transitionDirection="up"
-        />
-        <Calendar setIsClient={setIsClient} />
-        <Benefits />
-        <About />
-        <FAQ />
-        <CTA />
-        <Slogan
-          title={t('slogan.title')}
-          description={t('slogan.description')}
-        />
-        <Footer
-          email="contact@zardo.dev"
-          socialLinks={SOCIAL_LINKS}
-          onScrollToTop={() =>
-            scrollToSection({ sectionId: 'hero', offset: 80, duration: 800 })
-          }
-          backToTopLabel={t('footer.backToTop')}
-        />
-      </>
-    )}
-  </div>
-)}
+  return (
+    <div className="overflow-hidden bg-brand-offwhite relative">
+      {!isClient && (<LoadingScreen />)}
+      <LanguageSwitcher />
+      <Slogan
+        title={t('header.title')}
+        description={t('header.description')}
+        borderRadius="bottom"
+        transitionDirection="up"
+      />
+      <Calendar setIsClient={setIsClient} />
+      <Benefits />
+      <About />
+      <FAQ />
+      <CTA />
+      <Slogan
+        title={t('slogan.title')}
+        description={t('slogan.description')}
+      />
+      <Footer
+        email="contact@zardo.dev"
+        socialLinks={SOCIAL_LINKS}
+        onScrollToTop={() =>
+          scrollToSection({ sectionId: 'hero', offset: 80, duration: 800 })
+        }
+        backToTopLabel={t('footer.backToTop')}
+      />
+    </div>
+  )}
