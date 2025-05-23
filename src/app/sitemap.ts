56aaacda02190d0becf1c9e65c57d591a92f27b8
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-05-23 14:07:28 -0300

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,17 @@
-import { MetadataRoute } from "next"
+import { MetadataRoute } from "next";
 
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://zardo.dev/booking/sitemap.xml",
-  }
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://zardo.dev/booking";
+
+  const staticRoutes = [
+    '',
+    '/confirmation',
+  ].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly' as const,
+    priority: route === '' ? 1 : 0.8,
+  }));
+
+  return staticRoutes;
 }
